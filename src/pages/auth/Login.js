8165dba78e95a3163616c56686d2222d29992317
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-08-16 22:55:33 +0300

    16.08

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,37 +1,97 @@
 //// kayıtlı kullanıcılar için giriş sayfası
-import React from 'react'
-import styles from "./auth.module.scss"
-import loginImg from "../../assets/login.png"
-import { Link } from 'react-router-dom'
-import { FaGoogle } from 'react-icons/fa'
-import Card from '../../components/card/Card'
+import React, { useState } from "react";
+import styles from "./auth.module.scss";
+import loginImg from "../../assets/login.png";
+import { Link, useNavigate } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import Card from "../../components/card/Card";
+import { auth } from "../../firebase/config";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import { toast } from "react-toastify";
+import Loader from "../../components/loader/Loader";
 
 const Login = () => {
-  return (
-    <section className={`container ${styles.auth}`}>
-      <div className={styles.img}>
-        <img src={loginImg} alt="login" width="400"/>
-      </div>
-      <Card cardClass={styles.form}>
-        <h2>Login</h2>
-        <form>
-          <input type="text" placeholder='Email' required/>
-          <input type="password" placeholder='Password' required/>
-          <button className="--btn --btn-primary --btn-block">Login</button>
-          <div className={styles.links}>
-            <Link to="/reset">Reset Password</Link>
-          </div>
-          <p>-- or --</p>
-        </form>
-        <button className="--btn --btn-danger --btn-block"><FaGoogle color="#fff" /> &nbsp;Login With Google</button>
-        <span className={styles.register}>
-          <p>Don't have an account ? </p>
-          &nbsp;
-          <Link to="/register">Register</Link>
-        </span>
-      </Card>
-    </section>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const provider = new GoogleAuthProvider();
+  const navigate = useNavigate();
 
-export default Login
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    // console.log(email,password)
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setIsLoading(false);
+        const user = userCredential.user;
+        toast.success("Login successfully...");
+        navigate("/");
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
+  };
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        toast.success("Login Successfully..")
+        navigate("/")
+      })
+      .catch((error) => {
+        toast.error(error.message)
+      });
+  };
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={loginImg} alt="login" width="400" />
+        </div>
+        <Card cardClass={styles.form}>
+          <h2>Login</h2>
+          <form onSubmit={loginUser}>
+            <input
+              type="text"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="--btn --btn-primary --btn-block">Login</button>
+            <div className={styles.links}>
+              <Link to="/reset">Reset Password</Link>
+            </div>
+            <p>-- or --</p>
+          </form>
+          <button className="--btn --btn-danger --btn-block" onClick={signInWithGoogle}>
+            <FaGoogle color="#fff" /> &nbsp;Login With Google
+          </button>
+          <span className={styles.register}>
+            <p>Don't have an account ? </p>
+            &nbsp;
+            <Link to="/register">Register</Link>
+          </span>
+        </Card>
+      </section>
+    </>
+  );
+};
+
+export default Login;
