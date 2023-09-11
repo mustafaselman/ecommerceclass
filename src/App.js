78b1de305eccac99eb640eb5991eaa50d37e062a
mustafaselmanonal <mustafaselman.100@gmail.com>
2023-09-11 23:12:27 +0300

    11.09

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import CheckoutDetails from './pages/checkout/CheckoutDetails'
 import Checkout from './pages/checkout/Checkout'
 import CheckoutSuccess from './pages/checkout/CheckoutSuccess'
 import OrderHistory from './pages/orderHistory/OrderHistory'
+import OrderDetails from './pages/orderDetails/OrderDetails'
+import ReviewProducts from './components/reviewProducts/ReviewProducts'
 
 const App = () => {
   return (
@@ -37,6 +39,8 @@ const App = () => {
         <Route path="/checkout" element={<Checkout/>}/>
         <Route path="/checkout-success" element={<CheckoutSuccess/>}/>
         <Route path="/order-history" element={<OrderHistory/>}/>
+        <Route path="/order-details/:id" element={<OrderDetails/>}/>
+        <Route path="/review-product/:id" element={<ReviewProducts/>}/>
       </Routes>
       <Footer/>
       </BrowserRouter>
--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -11,11 +11,17 @@ import {
   DECREASE_CART,
   selectCartItems,
 } from "../../../redux/slice/cartSlice";
+import useFetchCollection from "../../../customHooks/useFetchCollection";
+import Card from "../../card/Card";
+import StarsRating from "react-star-rate";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const document = useFetchDocument("products", id);
+
+  const {data} = useFetchCollection("reviews")
+  const filteredReviews = data.filter((review) => review.productID === id)
 
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
@@ -79,6 +85,31 @@ const ProductDetails = () => {
             </div>
           </>
         )}
+        <Card cardClass={styles.card}>
+          <h3>Product Reviews</h3>
+          <div>
+            {filteredReviews.length === 0 ? (<p>There are no reviews for this product yet</p>) : (
+              <>
+              {filteredReviews.map((item,index)=>{
+                const {rate,review,reviewDate,userName} = item
+                return (
+                  <div key={index} className={styles.review}>
+                    <StarsRating value={rate}></StarsRating>
+                    <p>{review}</p>
+                    <span>
+                      <b>{reviewDate}</b>
+                    </span>
+                    <br/>
+                    <span>
+                      <b>by: {userName}</b>
+                    </span>
+                  </div>
+                )
+              })}
+              </>
+            )}
+          </div>
+        </Card>
       </div>
     </section>
   );
